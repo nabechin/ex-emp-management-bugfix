c11f0a6c588e23a3263d5f23b5c77df791020c5f
nabenabe0258 <nabenabe0258@gmail.com>
2019-08-19 17:27:34 +0900

    (1-2)初級 入力値エラー処理を作成しました

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,33 +12,44 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前は必須です")
 	private String name;
 	/** メールアドレス */
+	@Size(min = 1, max = 127, message = "Eメールは１文字以上１２７文字以内で記載してください")
+	@Email(message = "Eメールの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@Size(min = 1, max = 127, message = "パスワードは１文字以上１２７文字以内で記載してください")
 	private String password;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailAddress() {
 		return mailAddress;
 	}
+
 	public void setMailAddress(String mailAddress) {
 		this.mailAddress = mailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
 		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
 				+ "]";
 	}
-	
+
 }
